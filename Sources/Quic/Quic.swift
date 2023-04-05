142e893fa51ecec068bac3cbefdecfc92964c492
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-04 17:37:30 -0700

    Moved static Version definitions into Version.swift

--- Sources/Quic/Quic.swift
+++ Sources/Quic/Quic.swift
@@ -1,13 +1,6 @@
 //  Copyright Kenneth Laskoski. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0
 
-extension Version {
-  static let version1: Version = 1
-  static let versionDraft27: Version = 0xff00_001b
-  static let versionDraft28: Version = 0xff00_001c
-  static let versionDraft29: Version = 0xff00_001d
-}
-
 let currentVersion: Version = { .version1 }()
 
 let supportedVersions: [Version] = { [.version1] }()
