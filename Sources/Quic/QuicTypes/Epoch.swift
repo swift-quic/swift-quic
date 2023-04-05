2f9b26b9ab4bbdef9f4e75b670b1b9bc7e5ad27c
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-04 19:13:26 -0700

    Added Epoch enum Connection State tracking

---
+++ Sources/Quic/QuicTypes/Epoch.swift
@@ -0,0 +1,9 @@
+//  Copyright Kenneth Laskoski. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+
+/// Quic Connection Epoch
+public enum Epoch {
+  case Initial
+  case Handshake
+  case Application
+}
