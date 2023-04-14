8d15f52f69ff2a932c95ffdd2b7c56614f8a3312
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-14 09:40:31 -0700

    Added EncodePacketNumber and DecodePacketNumber tests based on RFC9000 Appendix A

--- Tests/QuicTests/QuicTests.swift
+++ Tests/QuicTests/QuicTests.swift
@@ -38,4 +38,36 @@ final class QuicTests: XCTestCase {
   func testMaxConnectionIDLength() throws {
     XCTAssertEqual(maxConnectionIDLength, 20)
   }
+  
+  /// https://datatracker.ietf.org/doc/html/rfc9000#section-a.2
+  func testEncodePacketNumber() throws {
+    let largestAcked = UInt64("abe8b3", radix: 16)!
+    let packetNumberToSend = UInt64("ac5c02", radix: 16)!
+    
+    let outstandingPackets = packetNumberToSend - largestAcked
+    XCTAssertEqual(outstandingPackets, 29_519)
+    
+    let pn = encodePacketNumber(fullPacketNumber: packetNumberToSend, largestAcked: largestAcked)
+    print(pn.hexString)
+    XCTAssertEqual(pn.count, 2)
+    
+    let packetNumberToSend2 = UInt64("ace8fe", radix: 16)!
+    
+    let outstandingPackets2 = packetNumberToSend2 - largestAcked
+    XCTAssertEqual(outstandingPackets2, 65_611)
+    
+    let pn2 = encodePacketNumber(fullPacketNumber: packetNumberToSend2, largestAcked: largestAcked)
+    print(pn2.hexString)
+    XCTAssertEqual(pn2.count, 3)
+  }
+  
+  /// https://datatracker.ietf.org/doc/html/rfc9000#section-a.3
+  func testDecodePacketNumber() throws {
+    let largestKnownPacketNumber = UInt64("a82f30ea", radix: 16)!
+    let currentPacketNumber = UInt64("9b32", radix: 16)!
+    
+    let decoded = decodePacketNumber(largest: largestKnownPacketNumber, truncated: currentPacketNumber, nBits: 16)
+    print(decoded)
+    XCTAssertEqual(decoded, UInt64("a82f9b32", radix: 16))
+  }
 }
