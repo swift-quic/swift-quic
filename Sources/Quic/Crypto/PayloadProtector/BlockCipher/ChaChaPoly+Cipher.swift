6aa464dc967ffd5db2d93d7fb30016d239be77d3
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-14 09:09:50 -0700

    Extending ChaChaPoly with QCipher conformance for use in a PacketProtector.

---
+++ Sources/Quic/Crypto/PayloadProtector/BlockCipher/ChaChaPoly+Cipher.swift
@@ -0,0 +1,33 @@
+
+import Crypto
+import Foundation
+
+public struct ChaChaPolyCipher:QBlockCipher {
+    typealias cipher = ChaChaPoly
+    let key:SymmetricKey
+    let iv:[UInt8]?
+    
+    init(key:SymmetricKey, iv:[UInt8]? = nil) {
+        self.key = key
+        self.iv = iv
+    }
+}
+
+extension Crypto.ChaChaPoly:QCipher {
+    static func encrypt<M, A>(message: M, using key: SymmetricKey, nonce:[UInt8]? = nil, authenticatingData: A) throws -> QSealedBox where M: DataProtocol, A: DataProtocol {
+        if let nonce = nonce {
+            let n = try ChaChaPoly.Nonce(data: nonce)
+            return try ChaChaPoly.seal(message, using: key, nonce: n, authenticating: authenticatingData)
+        }
+        return try ChaChaPoly.seal(message, using: key, authenticating: authenticatingData)
+    }
+
+    static func decrypt<A>(_ sealedBox: QSealedBox, using key: SymmetricKey, authenticatingData authenticatedData: A) throws -> [UInt8] where A : DataProtocol {
+      let sealed = try ChaChaPoly.SealedBox(nonce: ChaChaPoly.Nonce(data: sealedBox.qNonce), ciphertext: sealedBox.ciphertext, tag: sealedBox.tag)
+      return try ChaChaPoly.open(sealed, using: key, authenticating: authenticatedData).withUnsafeBytes { Array($0) }
+    }
+}
+
+extension Crypto.ChaChaPoly.SealedBox:QSealedBox {
+    var qNonce: [UInt8] { self.nonce.withUnsafeBytes { Array($0) } }
+}
