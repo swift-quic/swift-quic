71eb0a5d4b13ef77741c10ea67bb66193e7a95ed
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-14 09:21:32 -0700

    Added a bytesToRead computed prop to help simplify header decoding.

--- Sources/Quic/QuicTypes/ByteFragments/HeaderFragments/PacketNumberLength.swift
+++ Sources/Quic/QuicTypes/ByteFragments/HeaderFragments/PacketNumberLength.swift
@@ -6,6 +6,12 @@ enum PacketNumberLength: UInt8 {
   case _2 = 0b01
   case _4 = 0b10
   case _8 = 0b11
+  
+  public var bytesToRead:Int { Int(self.rawValue) + 1 }
+    
+  public init?(length:UInt8) {
+    self.init(rawValue: length - 1)
+  }
 }
 
 extension PacketNumberLength: ByteFragment {
