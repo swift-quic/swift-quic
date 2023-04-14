7f59737f5e810923172150139e6f9ba0edbc0fae
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-14 10:58:43 -0700

    Temporarily point to ChaCha20+Counter branch of swift-crypto

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swift-quic",
   platforms: [
-    .macOS(.v10_15),
+    .macOS(.v11),
     .iOS(.v13),
   ],
   products: [
@@ -16,7 +16,8 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
+    //.package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
+    .package(url: "https://github.com/btoms20/swift-crypto.git", branch: "feature/ChaCha20+Counter"),
   ],
   targets: [
     .target(
@@ -24,7 +25,8 @@ let package = Package(
       dependencies: [
         .product(name: "NIOCore", package: "swift-nio"),
         .product(name: "NIOPosix", package: "swift-nio"),
-        .product(name: "Crypto", package: "swift-crypto")
+        .product(name: "Crypto", package: "swift-crypto"),
+        .product(name: "_CryptoExtras", package: "swift-crypto"),
       ]
     ),
     .testTarget(
