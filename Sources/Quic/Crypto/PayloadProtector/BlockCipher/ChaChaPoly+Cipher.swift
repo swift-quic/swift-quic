235191209cc8e17af9b81e452d525c44fbc5d9f1
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 08:39:52 -0700

    Header and formatting

--- Sources/Quic/Crypto/PayloadProtector/BlockCipher/ChaChaPoly+Cipher.swift
+++ Sources/Quic/Crypto/PayloadProtector/BlockCipher/ChaChaPoly+Cipher.swift
@@ -1,20 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Crypto
 import Foundation
 
-public struct ChaChaPolyCipher:QBlockCipher {
+public struct ChaChaPolyCipher: QBlockCipher {
     typealias cipher = ChaChaPoly
-    let key:SymmetricKey
-    let iv:[UInt8]?
-    
-    init(key:SymmetricKey, iv:[UInt8]? = nil) {
+    let key: SymmetricKey
+    let iv: [UInt8]?
+
+    init(key: SymmetricKey, iv: [UInt8]? = nil) {
         self.key = key
         self.iv = iv
     }
 }
 
-extension Crypto.ChaChaPoly:QCipher {
-    static func encrypt<M, A>(message: M, using key: SymmetricKey, nonce:[UInt8]? = nil, authenticatingData: A) throws -> QSealedBox where M: DataProtocol, A: DataProtocol {
+extension Crypto.ChaChaPoly: QCipher {
+    static func encrypt<M, A>(message: M, using key: SymmetricKey, nonce: [UInt8]? = nil, authenticatingData: A) throws -> QSealedBox where M: DataProtocol, A: DataProtocol {
         if let nonce = nonce {
             let n = try ChaChaPoly.Nonce(data: nonce)
             return try ChaChaPoly.seal(message, using: key, nonce: n, authenticating: authenticatingData)
@@ -22,12 +35,12 @@ extension Crypto.ChaChaPoly:QCipher {
         return try ChaChaPoly.seal(message, using: key, authenticating: authenticatingData)
     }
 
-    static func decrypt<A>(_ sealedBox: QSealedBox, using key: SymmetricKey, authenticatingData authenticatedData: A) throws -> [UInt8] where A : DataProtocol {
-      let sealed = try ChaChaPoly.SealedBox(nonce: ChaChaPoly.Nonce(data: sealedBox.qNonce), ciphertext: sealedBox.ciphertext, tag: sealedBox.tag)
-      return try ChaChaPoly.open(sealed, using: key, authenticating: authenticatedData).withUnsafeBytes { Array($0) }
+    static func decrypt<A>(_ sealedBox: QSealedBox, using key: SymmetricKey, authenticatingData authenticatedData: A) throws -> [UInt8] where A: DataProtocol {
+        let sealed = try ChaChaPoly.SealedBox(nonce: ChaChaPoly.Nonce(data: sealedBox.qNonce), ciphertext: sealedBox.ciphertext, tag: sealedBox.tag)
+        return try ChaChaPoly.open(sealed, using: key, authenticating: authenticatedData).withUnsafeBytes { Array($0) }
     }
 }
 
-extension Crypto.ChaChaPoly.SealedBox:QSealedBox {
+extension Crypto.ChaChaPoly.SealedBox: QSealedBox {
     var qNonce: [UInt8] { self.nonce.withUnsafeBytes { Array($0) } }
 }
