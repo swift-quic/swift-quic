2956ca43ed3b7f86b2e62aae4b6be4b73a4b93e9
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 08:39:08 -0700

    Removed isLongHeader param from initializer

--- Sources/Quic/Crypto/HeaderProtector/HeaderProtector+ChaCha.swift
+++ Sources/Quic/Crypto/HeaderProtector/HeaderProtector+ChaCha.swift
@@ -1,16 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
-import Crypto
 import _CryptoExtras
+import Crypto
 import Foundation
 
 struct ChaChaHeaderProtector: HeaderProtector {
-  let sampleLength: Int = 16
-  var cipher: any QMaskCipher
-  var isLongHeader: Bool
+    let sampleLength: Int = 16
+    var cipher: any QMaskCipher
 
-  init(cipherSuite: CipherSuite, trafficSecret: SymmetricKey, isLongHeader: Bool, hkdfLabel: String) throws {
-    let hpKey = try cipherSuite.expandLabel(pseudoRandomKey: trafficSecret, label: hkdfLabel, outputByteCount: cipherSuite.keyLength)
-    self.cipher = ChaCha20CTRMaskCipher(key: hpKey)
-    self.isLongHeader = isLongHeader
-  }
+    init(cipherSuite: CipherSuite, trafficSecret: SymmetricKey, hkdfLabel: String) throws {
+        let hpKey = try cipherSuite.expandLabel(pseudoRandomKey: trafficSecret, label: hkdfLabel, outputByteCount: cipherSuite.keyLength)
+        self.cipher = ChaCha20CTRMaskCipher(key: hpKey)
+    }
 }
