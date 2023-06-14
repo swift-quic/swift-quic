d9df291d3439ca05b3e307b3317470b6d50c1b87
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 10:29:26 -0700

    Header and formatting.

--- Sources/Quic/QuicTypes/ConnectionID.swift
+++ Sources/Quic/QuicTypes/ConnectionID.swift
@@ -1,38 +1,53 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 struct ConnectionID: RawRepresentable {
-  typealias RawValue = [UInt8]
+    typealias RawValue = [UInt8]
 
-  private let data: RawValue
-  var rawValue: RawValue { data }
-  init(rawValue: RawValue) {
-    data = rawValue
-  }
+    private let data: RawValue
+    var rawValue: RawValue { self.data }
+    init(rawValue: RawValue) {
+        self.data = rawValue
+    }
 
-  var length: Int { data.count }
-  
-  /// Returns the ConnectionID's raw value prefixed with it's byte length as a UVarInt
-  var lengthPrefixedBytes:[UInt8] {
-    if length == 0 { return [0x00] }
-    return writeQuicVarInt(UInt64(self.length)) + self.data
-  }
+    var length: Int { self.data.count }
+
+    /// Returns the ConnectionID's raw value prefixed with it's byte length as a UVarInt
+    var lengthPrefixedBytes: [UInt8] {
+        if self.length == 0 { return [0x00] }
+        return writeQuicVarInt(UInt64(self.length)) + self.data
+    }
 }
 
 extension ConnectionID: ExpressibleByArrayLiteral {
-  init(arrayLiteral elements: RawValue.Element...) {
-    self.init(rawValue: elements)
-  }
+    init(arrayLiteral elements: RawValue.Element...) {
+        self.init(rawValue: elements)
+    }
+
+    init(randomOfLength length: Int) {
+        self.data = (0..<length).map { _ in UInt8.random(in: UInt8.min...UInt8.max) }
+    }
 }
 
 extension ConnectionID: QuicType {
-  init<S>(with bytes: S) where S: Sequence, S.Element == UInt8 {
-    self.init(rawValue: RawValue(bytes))
-  }
+    init<S>(with bytes: S) where S: Sequence, S.Element == UInt8 {
+        self.init(rawValue: RawValue(bytes))
+    }
 
-  func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
-    try rawValue.withUnsafeBytes(body)
-  }
+    func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
+        try self.rawValue.withUnsafeBytes(body)
+    }
 }
 
 extension ConnectionID: Codable {}
