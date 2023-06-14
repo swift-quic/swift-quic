1d5fccbd4cf92a73287b5e92c6ac6676c7986acf
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 10:05:20 -0700

    Header and formatting

--- Sources/Quic/NIO/Connection/Connection.swift
+++ Sources/Quic/NIO/Connection/Connection.swift
@@ -1,6 +1,17 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 protocol Connection {
-  func accept() async throws -> Stream
+    func accept() async throws -> Stream
 }
