5c7cfb7dffce3638c4f01d9af3f96fc3531b45d2
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 10:09:07 -0700

    Header and formatting

--- Sources/Quic/NIO/Stream/Stream.swift
+++ Sources/Quic/NIO/Stream/Stream.swift
@@ -1,20 +1,32 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
+import NIOCore
 
 protocol Stream {
-  var id: StreamID { get }
-  var type: StreamType { get }
-  var origin: EndpointRole { get }
-  var flowDirection: StreamFlowDirection { get }
+    var id: StreamID { get }
+    var type: StreamType { get }
+    var origin: EndpointRole { get }
+    var flowDirection: StreamFlowDirection { get }
 
-  func receive() async throws -> Data
-  func send(_ data: Data) async throws
+    func receive() async throws -> Data
+    func send(_ data: Data) async throws
 }
 
 extension Stream {
-  var type: StreamType { id.encodedType }
-  var origin: EndpointRole { type.origin }
-  var flowDirection: StreamFlowDirection { type.flowDirection }
+    var type: StreamType { id.encodedType }
+    var origin: EndpointRole { self.type.origin }
+    var flowDirection: StreamFlowDirection { self.type.flowDirection }
 }
