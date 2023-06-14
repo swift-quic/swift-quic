5fc1fa6b593cc1201c0f34a5e6b40ba489c3c07f
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 10:43:01 -0700

    Introduced Frame protocol

--- Sources/Quic/Quic.swift
+++ Sources/Quic/Quic.swift
@@ -1,25 +1,57 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIOCore
 
 let currentVersion: Version = { .version1 }()
 
 let supportedVersions: [Version] = { [.version1, .versionDraft29] }()
 
-let knownVersions: [Version] =  {
-  [
-    .version2,
-    .version1,
-    .versionDraft29,
-  ]
+let knownVersions: [Version] = {
+    [
+        .version2,
+        .version1,
+        .versionDraft29,
+    ]
 }()
 
 func isKnown(version: Version) -> Bool {
-  knownVersions.contains(version)
+    knownVersions.contains(version)
 }
 
 func isSupported(version: Version) -> Bool {
-  supportedVersions.contains(version)
+    supportedVersions.contains(version)
 }
 
 let minDatagramSize = { 1200 }()
 let maxConnectionIDLength = { 20 }()
+
+protocol Frame: Sendable {
+    var type: UInt8 { get }
+    func encode(into buffer: inout ByteBuffer)
+}
+
+extension Frame {
+    var serializedByteCount: Int {
+        self.serialized.readableBytes
+    }
+
+    var serialized: ByteBuffer {
+        var buf = ByteBuffer()
+        self.encode(into: &buf)
+        return buf
+    }
+}
+
+struct Frames { }
