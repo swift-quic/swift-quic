98f95a575872395edba96f86e7b68a16ce128733
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 11:07:18 -0700

    Preliminary Server State Handler (WIP)

--- Sources/Quic/NIO/Connection/Handlers/ServerHandler.swift
+++ Sources/Quic/NIO/Connection/Handlers/ServerHandler.swift
@@ -16,7 +16,7 @@ import Foundation
 import NIOCore
 import NIOSSL
 
-final class QUICServerHandler: ChannelDuplexHandler, QuicStateProvider, NIOSSLQuicDelegate {
+final class QUICServerHandler: ChannelDuplexHandler, NIOSSLQuicDelegate {
     // typealias changes to wrap out ByteBuffer in an AddressedEvelope which describes where the packages are going
     public typealias InboundIn = Packet
     public typealias InboundOut = ByteBuffer
