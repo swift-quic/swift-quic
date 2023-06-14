dca05d5cde6d0e51709f1fbaa8c142cb347329e0
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 10:34:05 -0700

    Array XOR helpers

--- Sources/Quic/Utilities/Array+XOR.swift
+++ Sources/Quic/Utilities/Array+XOR.swift
@@ -1,20 +1,31 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Foundation
 
 extension Array where Element == UInt8 {
-  mutating func xor(with other: [UInt8]) {
-    guard self.count <= other.count else { fatalError("Array must be of greater than or equal length to that of the XOR stream.") }
-    for i in 0..<self.count {
-      self[i] ^= other[i]
+    mutating func xor(with other: [UInt8]) {
+        guard self.count <= other.count else { fatalError("Array must be of greater than or equal length to that of the XOR stream.") }
+        for i in 0..<self.count {
+            self[i] ^= other[i]
+        }
     }
-  }
 
-  mutating func xorSubrange(from start: Int, to end: Int, with other: [UInt8]) {
-    guard start >= 0, end <= count, start <= end, end - start <= other.count else { fatalError("Invalid subrange or array length.") }
-    for i in start..<end {
-      self[i] ^= other[i - start]
+    mutating func xorSubrange(from start: Int, to end: Int, with other: [UInt8]) {
+        guard start >= 0, end <= count, start <= end, end - start <= other.count else { fatalError("Invalid subrange or array length.") }
+        for i in start..<end {
+            self[i] ^= other[i - start]
+        }
     }
-  }
 }
