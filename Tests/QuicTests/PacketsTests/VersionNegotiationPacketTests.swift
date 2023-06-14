878092b5ec4faac19f3cb43c6936cd714e855f9a
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 11:01:28 -0700

    Header and formatting

--- Tests/QuicTests/PacketsTests/VersionNegotiationPacketTests.swift
+++ Tests/QuicTests/PacketsTests/VersionNegotiationPacketTests.swift
@@ -1,29 +1,40 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import XCTest
 @testable import Quic
 
 final class VersionNegotiationPacketTests: XCTestCase {
-  func testVersionNegotiationPacket() throws {
-    let destinationID: ConnectionID = []
-    let sourceID: ConnectionID = [0]
-    let packet = VersionNegotiationPacket(destinationID: destinationID, sourceID: sourceID)
+    func testVersionNegotiationPacket() throws {
+        let destinationID: ConnectionID = []
+        let sourceID: ConnectionID = [0]
+        let packet = VersionNegotiationPacket(destinationID: destinationID, sourceID: sourceID)
 
-    XCTAssertEqual(packet.header.firstByte, 0b1100_0000)
-    XCTAssertEqual(packet.header.version, .negotiation)
-    XCTAssertEqual(packet.payload.count % MemoryLayout<Version>.size, 0)
+        XCTAssertEqual(packet.header.firstByte, 0b1100_0000)
+        XCTAssertEqual(packet.header.version, .negotiation)
+        XCTAssertEqual(packet.serializedPayload.count % MemoryLayout<Version>.size, 0)
 
-    XCTAssertEqual(packet.header.destinationIDLength, UInt8(destinationID.length))
-    XCTAssertEqual(packet.header.destinationID, destinationID)
-    XCTAssertEqual(packet.header.sourceIDLength, UInt8(sourceID.length))
-    XCTAssertEqual(packet.header.sourceID, sourceID)
+        XCTAssertEqual(packet.header.destinationIDLength, UInt8(destinationID.length))
+        XCTAssertEqual(packet.header.destinationID, destinationID)
+        XCTAssertEqual(packet.header.sourceIDLength, UInt8(sourceID.length))
+        XCTAssertEqual(packet.header.sourceID, sourceID)
 
-    let payload = supportedVersions.flatMap { version in
-      version.withUnsafeBytes { $0 }
+        let payload = supportedVersions.flatMap { version in
+            version.withUnsafeBytes { Array($0) }
+        }
+        XCTAssertEqual(packet.serializedPayload, payload)
+
+        XCTAssertEqual(packet.versions, supportedVersions)
     }
-    XCTAssertEqual(packet.payload, payload)
-
-    XCTAssertEqual(packet.versions, supportedVersions)
-  }
 }
