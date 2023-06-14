7c797c3ed16fdf4e1498ca5a5cb1cee6e307ea86
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 10:42:26 -0700

    Header and formatting

--- Sources/Quic/Utilities/UInt64+Bytes.swift
+++ Sources/Quic/Utilities/UInt64+Bytes.swift
@@ -1,8 +1,19 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 extension UInt64 {
-    func bytes(minBytes:Int = 0, bigEndian:Bool = true) -> [UInt8] {
+    func bytes(minBytes: Int = 0, bigEndian: Bool = true) -> [UInt8] {
         var bytes = Swift.withUnsafeBytes(of: bigEndian ? self.bigEndian : self.littleEndian, Array.init)
         while !bytes.isEmpty && bytes[0] == 0 && bytes.count > minBytes { bytes.removeFirst() }
         return bytes
@@ -10,7 +21,7 @@ extension UInt64 {
 }
 
 extension UInt32 {
-    func bytes(minBytes:Int = 0, bigEndian:Bool = true) -> [UInt8] {
+    func bytes(minBytes: Int = 0, bigEndian: Bool = true) -> [UInt8] {
         var bytes = Swift.withUnsafeBytes(of: bigEndian ? self.bigEndian : self.littleEndian, Array.init)
         while !bytes.isEmpty && bytes[0] == 0 && bytes.count > minBytes { bytes.removeFirst() }
         return bytes
@@ -18,38 +29,38 @@ extension UInt32 {
 }
 
 extension UInt64 {
-  init<T: Collection>(bytes: T) where T.Element == UInt8, T.Index == Int {
-    precondition(bytes.count == 8, "UInt64 requires 8 bytes of info")
-    
-    // We break this into two parts to help the compiler type check it
-    let startIndex = bytes.startIndex
-    let val0 = (
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 0)]) <<  0) |
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 1)]) <<  8) |
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 2)]) << 16) |
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 3)]) << 24)
-    )
-    let val1 = (
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 4)]) << 32) |
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 5)]) << 40) |
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 6)]) << 48) |
-      (UInt64(bytes[bytes.index(startIndex, offsetBy: 7)]) << 56)
-    )
-  
-    self = val0 | val1
-  }
+    init<T: Collection>(bytes: T) where T.Element == UInt8, T.Index == Int {
+        precondition(bytes.count == 8, "UInt64 requires 8 bytes of info")
+
+        // We break this into two parts to help the compiler type check it
+        let startIndex = bytes.startIndex
+        let val0 = (
+            (UInt64(bytes[bytes.index(startIndex, offsetBy: 0)]) << 0) |
+                (UInt64(bytes[bytes.index(startIndex, offsetBy: 1)]) << 8) |
+                (UInt64(bytes[bytes.index(startIndex, offsetBy: 2)]) << 16) |
+                (UInt64(bytes[bytes.index(startIndex, offsetBy: 3)]) << 24)
+        )
+        let val1 = (
+            (UInt64(bytes[bytes.index(startIndex, offsetBy: 4)]) << 32) |
+                (UInt64(bytes[bytes.index(startIndex, offsetBy: 5)]) << 40) |
+                (UInt64(bytes[bytes.index(startIndex, offsetBy: 6)]) << 48) |
+                (UInt64(bytes[bytes.index(startIndex, offsetBy: 7)]) << 56)
+        )
+
+        self = val0 | val1
+    }
 }
 
 extension UInt32 {
-  init<T: Collection>(bytes: T) where T.Element == UInt8, T.Index == Int {
-    precondition(bytes.count == 4, "UInt32 requires 4 bytes of info")
-    
-    let startIndex = bytes.startIndex
-    self = (
-       (UInt32(bytes[bytes.index(startIndex, offsetBy: 0)]) <<  0) |
-       (UInt32(bytes[bytes.index(startIndex, offsetBy: 1)]) <<  8) |
-       (UInt32(bytes[bytes.index(startIndex, offsetBy: 2)]) << 16) |
-       (UInt32(bytes[bytes.index(startIndex, offsetBy: 3)]) << 24)
-    )
-  }
+    init<T: Collection>(bytes: T) where T.Element == UInt8, T.Index == Int {
+        precondition(bytes.count == 4, "UInt32 requires 4 bytes of info")
+
+        let startIndex = bytes.startIndex
+        self = (
+            (UInt32(bytes[bytes.index(startIndex, offsetBy: 0)]) << 0) |
+                (UInt32(bytes[bytes.index(startIndex, offsetBy: 1)]) << 8) |
+                (UInt32(bytes[bytes.index(startIndex, offsetBy: 2)]) << 16) |
+                (UInt32(bytes[bytes.index(startIndex, offsetBy: 3)]) << 24)
+        )
+    }
 }
