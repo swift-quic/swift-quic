99543f5218dbb1b784e797a7eec79e073374ae38
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-14 08:42:52 -0700

    Header and formatting

--- Sources/Quic/Crypto/SymmetricKey+Bytes.swift
+++ Sources/Quic/Crypto/SymmetricKey+Bytes.swift
@@ -1,9 +1,21 @@
-
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import Crypto
 
 extension SymmetricKey {
-    var bytes:[UInt8] {
+    var bytes: [UInt8] {
         self.withUnsafeBytes { Array($0) }
     }
 }
