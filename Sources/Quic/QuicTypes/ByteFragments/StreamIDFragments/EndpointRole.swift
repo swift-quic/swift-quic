2987d3d48879e73a41de8e2516558a5d19170167
Brandon <32753167+btoms20@users.noreply.github.com>
2023-06-30 13:14:51 -0700

    Added opposite helper var

--- Sources/Quic/QuicTypes/ByteFragments/StreamIDFragments/EndpointRole.swift
+++ Sources/Quic/QuicTypes/ByteFragments/StreamIDFragments/EndpointRole.swift
@@ -1,13 +1,33 @@
-//  Copyright Kenneth Laskoski. All Rights Reserved.
-//  SPDX-License-Identifier: Apache-2.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftQUIC open source project
+//
+// Copyright (c) 2023 the SwiftQUIC project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftQUIC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 enum EndpointRole: UInt8 {
-  case client = 0
-  case server = 1
+    case client = 0
+    case server = 1
 }
 
 extension EndpointRole: ByteFragment {
-  static let mask: UInt8 = 1
+    static let mask: UInt8 = 1
+}
+
+extension EndpointRole {
+    var opposite: EndpointRole {
+        switch self {
+            case .client: return .server
+            case .server: return .client
+        }
+    }
 }
 
 extension EndpointRole {
