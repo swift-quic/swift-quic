43a6766c906c5cc4f3a570375355e790b43dbe07
Brandon <32753167+btoms20@users.noreply.github.com>
2023-07-06 21:07:25 -0700

    Made ReadResult generic. Reduced repetitive code during TLS frame decoding.

--- Sources/Quic/NIO/Connection/ByteBuffer+Decoding.swift
+++ Sources/Quic/NIO/Connection/ByteBuffer+Decoding.swift
@@ -143,12 +143,12 @@ extension ByteBuffer {
 }
 
 extension ByteBuffer {
-    public enum ReadResult {
+    public enum ReadResult<T> {
         case invalidFrame
         case needMoreData
-        case success([UInt8])
+        case success(T)
     }
-    
+
     /// Attempts to consume a Quic Crypto Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `CryptoFrame`s bytes or `nil` if there wasn't a valid frame available.
@@ -187,19 +187,19 @@ extension ByteBuffer {
     ///
     /// - returns: A `[UInt8]` value containing the `TLS ServerHello`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public mutating func readTLSClientHello() -> ReadResult {
+    public mutating func readTLSClientHello() -> ReadResult<[UInt8]> {
         let result = self.getTLSClientHello()
         if case .success(let clientHello) = result {
             self.moveReaderIndex(forwardBy: clientHello.count)
         }
         return result
     }
-    
+
     /// Attempts to consume a TLS ServerHello Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `TLS ServerHello`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public mutating func readTLSServerHello() -> ReadResult {
+    public mutating func readTLSServerHello() -> ReadResult<[UInt8]> {
         let result = self.getTLSServerHello()
         if case .success(let serverHello) = result {
             self.moveReaderIndex(forwardBy: serverHello.count)
@@ -211,7 +211,7 @@ extension ByteBuffer {
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Encrypted Extension`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public mutating func readTLSEncryptedExtensions() -> ReadResult {
+    public mutating func readTLSEncryptedExtensions() -> ReadResult<[UInt8]> {
         let result = self.getTLSEncryptedExtensions()
         if case .success(let encryptedExtensions) = result {
             self.moveReaderIndex(forwardBy: encryptedExtensions.count)
@@ -223,7 +223,7 @@ extension ByteBuffer {
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Certificate`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public mutating func readTLSCertificate() -> ReadResult {
+    public mutating func readTLSCertificate() -> ReadResult<[UInt8]> {
         let result = self.getTLSCertificate()
         if case .success(let certificate) = result {
             self.moveReaderIndex(forwardBy: certificate.count)
@@ -235,7 +235,7 @@ extension ByteBuffer {
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Certificate Verify`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public mutating func readTLSCertificateVerify() -> ReadResult {
+    public mutating func readTLSCertificateVerify() -> ReadResult<[UInt8]> {
         let result = self.getTLSCertificateVerify()
         if case .success(let certVerify) = result {
             self.moveReaderIndex(forwardBy: certVerify.count)
@@ -247,7 +247,7 @@ extension ByteBuffer {
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Certificate Verify`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public mutating func readTLSHandshakeFinished() -> ReadResult {
+    public mutating func readTLSHandshakeFinished() -> ReadResult<[UInt8]> {
         let result = self.getTLSHandshakeFinished()
         if case .success(let finished) = result {
             self.moveReaderIndex(forwardBy: finished.count)
@@ -495,114 +495,167 @@ extension ByteBuffer {
     ///
     /// - returns: A `[UInt8]` value containing the `TLS ClientHello`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public func getTLSClientHello() -> ReadResult {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x01, 0x00] else { return .invalidFrame }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return .invalidFrame }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
-            return .needMoreData
-        }
-        return .success(result)
+    public func getTLSClientHello() -> ReadResult<[UInt8]> {
+        return self.getTLS(frame: .clientHello)
     }
 
     public func getTLSClientHelloSlice() -> ByteBuffer? {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x01, 0x00] else { return nil }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return nil }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getSlice(at: self.readerIndex, length: bytesToConsume) else {
-            return nil
-        }
-        return result
+        return self.optionallyUnwraped(self.getTLSSlice(frame: .clientHello))
     }
 
     /// Attempts to consume a TLS ServerHello Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `TLS ServerHello`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public func getTLSServerHello() -> ReadResult {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x02, 0x00] else { return .invalidFrame }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return .invalidFrame }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
-            return .needMoreData
-        }
-        return .success(result)
+    public func getTLSServerHello() -> ReadResult<[UInt8]> {
+        return self.getTLS(frame: .serverHello)
     }
 
     public func getTLSServerHelloSlice() -> ByteBuffer? {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x02, 0x00] else { return nil }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return nil }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getSlice(at: self.readerIndex, length: bytesToConsume) else {
-            return nil
-        }
-        return result
+        return self.optionallyUnwraped(self.getTLSSlice(frame: .serverHello))
     }
 
     /// Attempts to consume a TLS Encrypted Extensions Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Encrypted Extension`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public func getTLSEncryptedExtensions() -> ReadResult {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x08, 0x00] else { return .invalidFrame }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return .invalidFrame }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
-            return .needMoreData
-        }
-        return .success(result)
+    public func getTLSEncryptedExtensions() -> ReadResult<[UInt8]> {
+        return self.getTLS(frame: .encryptedExtensions)
     }
 
     public func getTLSEncryptedExtensionsSlice() -> ByteBuffer? {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x08, 0x00] else { return nil }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return nil }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getSlice(at: self.readerIndex, length: bytesToConsume) else {
-            return nil
-        }
-        return result
+        return self.optionallyUnwraped(self.getTLSSlice(frame: .encryptedExtensions))
     }
 
     /// Attempts to consume a TLS Certificate Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Certificate`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public func getTLSCertificate() -> ReadResult {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x0b, 0x00] else { return .invalidFrame }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return .invalidFrame }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
-            return .needMoreData
-        }
-        return .success(result)
+    public func getTLSCertificate() -> ReadResult<[UInt8]> {
+        return self.getTLS(frame: .certificate)
     }
 
     /// Attempts to consume a TLS Certificate Verify Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Certificate Verify`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public func getTLSCertificateVerify() -> ReadResult {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x0f, 0x00] else { return .invalidFrame }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return .invalidFrame }
-        let bytesToConsume = 4 + Int(length)
-        guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
-            return .needMoreData
-        }
-        return .success(result)
+    public func getTLSCertificateVerify() -> ReadResult<[UInt8]> {
+        return self.getTLS(frame: .certificateVerify)
     }
 
     /// Attempts to consume a TLS Certificate Verify Frame and returns the bytes upon success
     ///
     /// - returns: A `[UInt8]` value containing the `TLS Certificate Verify`s bytes or `nil` if there wasn't a valid frame available.
     //@inlinable
-    public func getTLSHandshakeFinished() -> ReadResult {
-        guard self.getBytes(at: self.readerIndex, length: 2) == [0x14, 0x00] else { return .invalidFrame }
-        guard let length = self.getInteger(at: self.readerIndex + 2, as: UInt16.self) else { return .invalidFrame }
+    public func getTLSHandshakeFinished() -> ReadResult<[UInt8]> {
+        return self.getTLS(frame: .finished)
+    }
+
+    /// https://datatracker.ietf.org/doc/html/rfc8446#appendix-B.3
+    public enum TLSFrame: UInt8 {
+        case helloRequestRESERVED = 0
+        case clientHello = 1
+        case serverHello = 2
+        case helloVerifyRequestRESERVED = 3
+        case newSessionTicket = 4
+        case endOfEarlyData = 5
+        case helloRetryRequestRESERVED = 6
+        case encryptedExtensions = 8
+        case certificate = 11
+        case serverKeyExchangeRESERVED = 12
+        case certificateRequest = 13
+        case serverHelloDoneRESERVED = 14
+        case certificateVerify = 15
+        case clientKeyExchangeRESERVED = 16
+        case finished = 20
+        case certificateUrlRESERVED = 21
+        case certificateStatusRESERVED = 22
+        case supplementalDataRESERVED = 23
+        case keyUpdate = 24
+        case messageHash = 254
+    }
+
+    // TODO: The length prefix is technically a UInt24
+    internal func getTLSFrameHeader(at: Int) -> ReadResult<(TLSFrame, Int)> {
+        guard let tag = self.getBytes(at: at, length: 1)?.first else { return .needMoreData }
+        guard let type = TLSFrame(rawValue: tag) else { return .invalidFrame }
+        guard let length = self.getInteger(at: at + 2, as: UInt16.self) else { return .invalidFrame }
         let bytesToConsume = 4 + Int(length)
-        guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
-            return .needMoreData
+        return .success((type, bytesToConsume))
+    }
+
+    public func getTLS(frame: TLSFrame) -> ReadResult<[UInt8]> {
+        switch self.getTLSFrameHeader(at: self.readerIndex) {
+            case .invalidFrame: return .invalidFrame
+            case .needMoreData: return .needMoreData
+            case .success((let type, let bytesToConsume)):
+                guard type == frame else { return .invalidFrame }
+                guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
+                    return .needMoreData
+                }
+                return .success(result)
         }
-        return .success(result)
+    }
+
+    public func getTLSSlice(frame: TLSFrame) -> ReadResult<ByteBuffer> {
+        switch self.getTLSFrameHeader(at: self.readerIndex) {
+            case .invalidFrame: return .invalidFrame
+            case .needMoreData: return .needMoreData
+            case .success((let type, let bytesToConsume)):
+                guard type == frame else { return .invalidFrame }
+                guard let result = self.getSlice(at: self.readerIndex, length: bytesToConsume) else {
+                    return .needMoreData
+                }
+                return .success(result)
+        }
+    }
+
+    public mutating func readTLS(frame: TLSFrame) -> ReadResult<[UInt8]> {
+        let res = self.getTLS(frame: frame)
+        if case .success(let r) = res {
+            self.moveWriterIndex(forwardBy: r.count)
+        }
+        return res
+    }
+
+    public func getTLSFrame() -> ReadResult<(TLSFrame, [UInt8])> {
+        switch self.getTLSFrameHeader(at: self.readerIndex) {
+            case .invalidFrame: return .invalidFrame
+            case .needMoreData: return .needMoreData
+            case .success((let type, let bytesToConsume)):
+                guard let result = self.getBytes(at: self.readerIndex, length: bytesToConsume) else {
+                    return .needMoreData
+                }
+                return .success((type, result))
+        }
+    }
+
+    public func getTLSFrameSlice() -> ReadResult<(TLSFrame, ByteBuffer)> {
+        switch self.getTLSFrameHeader(at: self.readerIndex) {
+            case .invalidFrame: return .invalidFrame
+            case .needMoreData: return .needMoreData
+            case .success((let type, let bytesToConsume)):
+                guard let result = self.getSlice(at: self.readerIndex, length: bytesToConsume) else {
+                    return .needMoreData
+                }
+                return .success((type, result))
+        }
+    }
+
+    public mutating func readTLSFrame() -> ReadResult<(TLSFrame, [UInt8])> {
+        let res = self.getTLSFrame()
+        if case .success(let r) = res {
+            self.moveWriterIndex(forwardBy: r.1.count)
+        }
+        return res
+    }
+
+    private func optionallyUnwraped<T>(_ result: ReadResult<T>) -> T? {
+        switch result {
+            case .invalidFrame, .needMoreData: return nil
+            case .success(let t):
+                return t
+        }
     }
 
     public func getQuicCryptoFrameContents() -> [UInt8]? {
